13dfb04f238c4097c122201d36da56113fc731e2
astasia <olek.stasiak@gmail.com>
2016-05-30 20:45:59 +0200

    printout disabled

--- fpj-monitor/src/main/java/com/fpj/spring/service/PrintOutJob.java
+++ fpj-monitor/src/main/java/com/fpj/spring/service/PrintOutJob.java
@@ -1,13 +1,11 @@
 package com.fpj.spring.service;
 
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class PrintOutJob {
 
 	private int i=0;
-	@Scheduled(cron="0 * * * * *")
+	//@Scheduled(cron="0 * * * * *")
 	void print(){
 		System.out.println("Run no: " + ++i );
 	}
